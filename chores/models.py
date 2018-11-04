bdefd68fc6078e19c44f671b57e6abd27d37b306
Cidney Weng <cidney.weng@gmail.com>
2018-11-03 21:56:17 -0700

    models 2

--- chores/models.py
+++ chores/models.py
@@ -19,10 +19,18 @@ class Task(models.Model):
 class Person(models.Model):
     username = models.CharField(max_length = 15)
     passworld = models.CharField(max_length = 25)
+    name = models.CharField(max_length = 15)
     color = models.CharField(max_length = 7)
     tasks = []
     friends = []
 
     def addTask(self, task):
         tasks.append(task)
-        
+        self.save()
+
+    def addFriend(self, friend):
+        friends.append(friend)
+        self.save()
+
+    def __str__(self):
+        return self.name
