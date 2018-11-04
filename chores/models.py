b76abd87b2e032dc7d7eac3c037c9c63a032c0de
Cidney Weng <cidney.weng@gmail.com>
2018-11-03 21:20:30 -0700

    models 1

--- chores/models.py
+++ chores/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    due_date = models.DateTimeField(blank = True, null = True)
+    completed = models.BooleanField(default = False)
+
+    def complete(self):
+        self.completed = True
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Person(models.Model):
+    username = models.CharField(max_length = 15)
+    passworld = models.CharField(max_length = 25)
+    color = models.CharField(max_length = 7)
+    tasks = []
+    friends = []
+
+    def addTask(self, task):
+        tasks.append(task)
+        
